b8ea269b6335cfd4de0e4b86ede6167b9c8317ac
biac1205 <biancamosquera1205@gmail.com>
2022-06-22 20:00:47 -0500

    codigojs
    prueba

---
+++ main.js
@@ -0,0 +1,34 @@
+ 
+ let paneles = "grandes";
+
+ if (paneles === "grandes"){
+     alert("son grandes")
+ }else if(paneles === "pequeños"){
+     alert("oiga no son grandes")
+ }else{
+     alert("no nos sirven")
+ }
+
+
+let hierba = "mediana";
+
+ if (hierba === "mediana"){
+     alert("cortar 2cm")
+ }else if(hierba === "alta"){
+     alert("cortar 4cm")
+ }else{
+     alert("no cortar porque esta pequeño")
+ }
+
+let num1=5;
+let num2=18;
+
+if (num1 < num2){
+    alert("puedes entrar")
+}else if(num1 === num2){
+    alert("ni se diga, ingresa")
+}else{
+    alert("no entrar")
+}
+
+ 
